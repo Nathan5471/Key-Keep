9c7427693a29c7a1a2fb1dc50421b60e28fe8520
Nathan5471 <nathanc4571@gmail.com>
2023-06-24 15:31:34 -0400

    Initial commit

---
+++ Main.py
@@ -0,0 +1,34 @@
+from Password_Generator import *
+
+
+def generate_login():
+    website = input("What is the website you are creating a login for?: ")
+    username = input("What is the username you are using for this login?: ")
+    password = generate_password()
+    print(password)
+    done = True
+    while done:
+        good_password = input("Is this password okay? y or n: ")
+        if good_password == "y":
+            with open("Passwords.txt", "a") as file:
+                file.write("Website: " + website + "\n")
+                file.write("Username: " + username + "\n")
+                file.write("Password: " + password + "\n" + "\n")
+            done = False
+        elif good_password == "n":
+            password = generate_password()
+            print(password)
+        else:
+            good_password = input("Please input y or n: ")
+
+
+def get_websites():
+    with open("Passwords.txt", "r") as file:
+        for line in file:
+            if "Website: " in line:
+                unwanted_text = "Website: "
+                print(line.replace(unwanted_text, ""))
+
+def get_login():
+    with open("Passwords.txt", "r") as file:
+        
