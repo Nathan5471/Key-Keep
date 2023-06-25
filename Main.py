9348c69cc9570ed551232d7dbdd49ec454d71563
Nathan5471 <nathanc4571@gmail.com>
2023-06-25 19:32:39 -0400

    Added ability to edit login data

--- Main.py
+++ Main.py
@@ -38,13 +38,61 @@ def get_websites():
                 print(line.replace(unwanted_text, ""))
 
 
-def get_login():
-    website = input("What website do you want your login information from?: ")
+def get_login(edit="", website=""):
+    if edit == "true":
+        with open("Passwords.txt", "r") as file:
+            lines = file.readlines()
+            num_lines = len(lines)
+            for i in range(num_lines):
+                line = lines[i]
+                if website in line:
+                    return lines[i + 1] + lines[i + 2]
+    else:
+        website = input("What website do you want your login information from?: ")
+        with open("Passwords.txt", "r") as file:
+            lines = file.readlines()
+            num_lines = len(lines)
+            for i in range(num_lines):
+                line = lines[i]
+                if website in line:
+                    print(lines[i + 1])
+                    print(lines[i + 2])
+
+
+def edit_login():
+    edit_website = input(
+        "What website do you want to change the login information for?: "
+    )
+    edit_user = input("Do you want to edit your username? y or n: ")
+    if edit_user == "y" or edit_user == "n":
+        valid_response = True
+    else:
+        valid_response = False
+        while not valid_response:
+            edit_user = input("Please input y or n: ")
+            if edit_user == "y" or "n":
+                valid_response = True
+    edit_password = input("Do you want to edit your password? y or n: ")
+    if edit_password == "y" or edit_password == "n":
+        valid_response = True
+    else:
+        valid_response = False
+        while not valid_response:
+            edit_password = input("Please input y or n: ")
+            if edit_password == "y" or "n":
+                valid_response = True
+    if edit_user == "y":
+        new_user = input("What do you want your new username to be?: ")
+    if edit_password == "y":
+        new_password = input("what do you want your new password to be?: ")
+    login_info = get_login("true", edit_website)
     with open("Passwords.txt", "r") as file:
-        lines = file.readlines()
-        num_lines = len(lines)
-        for i in range(num_lines):
-            line = lines[i]
-            if website in line:
-                print(lines[i + 1])
-                print(lines[i + 2])
+        logins = file.read()
+
+    update_login = logins.replace(
+        login_info,
+        "Username: " + new_user + "\nPassword: " + new_password + "\n" + "\n",
+    )
+
+    with open("Passwords.txt", "w") as file:
+        file.write(update_login)
